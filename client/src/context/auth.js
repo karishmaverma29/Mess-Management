0be15cbb2f03936b911a69bcc4c3b0d1bfc1fdd4
Dinesh <dineshkumarpaliwal83@gmail.com>
2023-11-06 23:10:21 +0530

    context api and cookie created

---
+++ client/src/context/auth.js
@@ -0,0 +1,35 @@
+import { useState, useEffect, useContext, createContext } from "react";
+import Cookies from "js-cookie";
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState({
+    user: null,
+    token: "",
+  });
+
+  useEffect(() => {
+    const data = Cookies.get("auth");
+    if (data) {
+      const parseData = JSON.parse(data);
+      console.log("Cookie data:", parseData);
+      setAuth({
+        ...auth,
+        user: parseData.user,
+        token: parseData.token,
+      });
+    }
+    //eslint-disable-next-line
+  }, []);
+
+  return (
+    <AuthContext.Provider value={[auth, setAuth]}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+// custom hook
+const useAuth = () => useContext(AuthContext);
+
+export { useAuth, AuthProvider };
--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -6,45 +6,59 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "../../styles/AuthStyles.css";
 import { useLocation } from "react-router-dom";
+import Cookies from "js-cookie";
 
-
-
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+//dropdown of login as ........
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { useAuth } from "../../context/auth";
 
 const Login = () => {
+  const [auth, setAuth] = useAuth();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [role,setRole]=useState("");
+  const [role, setRole] = useState("");
   const navigate = useNavigate();
   const location = useLocation();
 
-  
-   
   const handleChange = (event) => {
     setRole(event.target.value);
   };
-  
-
-
 
   // form function
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-        console.log(role);
-        let res;
-        if(role==0)  res = await axios.post("/api/v1/auth/userlogin", {email,password,role});
-        else  res = await axios.post("/api/v1/auth/adminlogin", {email,password,role});
-     
+      console.log(role);
+      let res;
+      if (role == 0)
+        res = await axios.post("/api/v1/auth/userlogin", {
+          email,
+          password,
+          role,
+        });
+      else
+        res = await axios.post("/api/v1/auth/adminlogin", {
+          email,
+          password,
+          role,
+        });
+
       //checking email and password
-      //if ok then fill the auth with user data and token 
+      //if ok then fill the auth with user data and token
 
       if (res && res.data.success) {
         toast.success(res.data && res.data.message);
-        //storing in local storage
+        setAuth({
+          ...auth,
+          user: res.data.user,
+          token: res.data.token,
+        });
+        // Store data in a cookie
+        Cookies.set("auth", JSON.stringify(res.data), { expires: 7 });
+
         navigate(location.state || "/");
       } else {
         toast.message(res.data.message);
@@ -84,8 +98,8 @@ const Login = () => {
                 required
               />
             </div>
-            
-         {/* <div>
+
+            {/* <div>
            <div className="form-check">
              <input className="form-check-input" type="radio" name="flexRadioDefault"   onClick={() => setRole(0)} />
              <label className="form-check-label" htmlFor="flexRadioDefault1">
@@ -113,31 +127,26 @@ const Login = () => {
            </div>
          </div>  */}
 
-
-<div>
-      <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-        <InputLabel id="demo-select-small-label">Login As</InputLabel>
-        <Select
-          labelId="demo-select-small-label"
-          id="demo-select-small"
-          value={role}
-          label="Login As"
-          onChange={handleChange}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={0}>Student</MenuItem>
-          <MenuItem value={1}>Warden</MenuItem>
-          <MenuItem value={2}>Accountant</MenuItem>
-          <MenuItem value={3}>Manager</MenuItem>
-        </Select>
-      </FormControl>
-    </div>
- 
-
-
-
+            <div>
+              <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+                <InputLabel id="demo-select-small-label">Login As</InputLabel>
+                <Select
+                  labelId="demo-select-small-label"
+                  id="demo-select-small"
+                  value={role}
+                  label="Login As"
+                  onChange={handleChange}
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={0}>Student</MenuItem>
+                  <MenuItem value={1}>Warden</MenuItem>
+                  <MenuItem value={2}>Accountant</MenuItem>
+                  <MenuItem value={3}>Manager</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
 
             {/* <div className="mb-3">
               <button
